8a0ed24beec966a7c7df6662b9449634fc1dc0fa
ADyBaH <Brandon4@mail.ru>
2023-07-11 19:04:43 +0300

    refactor: change method init

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,18 +2,13 @@ import { BaseComponent } from '../utils/base-component'
 import { Layout } from './layouts/layout'
 
 class App {
-  public layout = new Layout()
   private root = new BaseComponent({
     attribute: { id: 'root', className: 'root' },
     parent: document.body,
   })
 
-  public init(): boolean {
-    if (!this.root) {
-      return false
-    }
-    this.layout.create(this.root.element)
-    return true
+  public init(): void {
+    new Layout().create(this.root.element)
   }
 }
 
